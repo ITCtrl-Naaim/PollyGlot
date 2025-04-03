8f5aa0b8a3e9c954d993a75d073887841189a99a
itcrl-naaim <naaimkraiem@gmail.com>
2025-04-03 20:25:11 +0300

    API key verification

--- translation.js
+++ translation.js
@@ -2,6 +2,12 @@ import { HfInference } from "@huggingface/inference";
 const API_KEY = import.meta.env.VITE_HF_API_KEY;
 // import { HfInference } from "https://cdn.skypack.dev/@huggingface/inference";
 
+if (!API_KEY) {
+  console.error(
+    "Hugging Face API token is missing. Please set VITE_HUGGINGFACE_API_TOKEN in your environment."
+  );
+}
+
 const hf = new HfInference(API_KEY);
 
 export async function translateText(text = "Hello", language = "fr") {
